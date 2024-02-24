6cb45cb79669a4b6d6029928efd4e4bb86a5a532
G00438454 <g00438454@atu.ie>
2024-02-24 16:28:50 +0000

    css for property for sale page

---
+++ src/temp-data.js
@@ -0,0 +1,69 @@
+import house1 from './assets/house1.jpg'
+import house2 from './assets/house2.jpg'
+import house3 from './assets/house3.jpg'
+import house4 from './assets/house4.jpg'
+import house5 from './assets/house5.jpg'
+import house6 from './assets/house6.jpg'
+
+export const houses = [
+    {
+      id: 1,
+      houseName: "Cozy Cottage",
+      houseAddress: "123 Elm Street",
+      price: 200000,
+      numOfBedroom: 2,
+      numOfBathroom: 1,
+      sqft: 1200,
+      image: house1,
+    },
+    {
+      id: 2,
+      houseName: "Modern Mansion",
+      houseAddress: "456 Oak Avenue",
+      price: 1000000,
+      numOfBedroom: 5,
+      numOfBathroom: 4,
+      sqft: 5000,
+      image: house2,
+    },
+    {
+      id: 3,
+      houseName: "Rustic Retreat",
+      houseAddress: "789 Pine Road",
+      price: 350000,
+      numOfBedroom: 3,
+      numOfBathroom: 2,
+      sqft: 2000,
+      image: house3,
+    },
+    {
+      id: 4,
+      houseName: "Lakeside Lodge",
+      houseAddress: "1010 Lakeview Drive",
+      price: 600000,
+      numOfBedroom: 4,
+      numOfBathroom: 3,
+      sqft: 3500,
+      image: house4,
+    },
+    {
+      id: 5,
+      houseName: "Suburban Sanctuary",
+      houseAddress: "1313 Maple Lane",
+      price: 450000,
+      numOfBedroom: 3,
+      numOfBathroom: 2,
+      sqft: 2200,
+      image: house5,
+    },
+    {
+      id: 6,
+      houseName: "City Loft",
+      houseAddress: "1515 Broadway Avenue",
+      price: 800000,
+      numOfBedroom: 2,
+      numOfBathroom: 2,
+      sqft: 1800,
+      image: house6,
+    }
+  ];
