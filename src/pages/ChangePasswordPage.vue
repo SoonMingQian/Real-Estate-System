88a5c907ab5c2bccad6e1f4d1ce1f1369ba4b066
G00438454 <g00438454@atu.ie>
2024-04-10 12:20:09 +0100

    design the changepassword page and forgot password page and add usermanagement page

--- src/pages/ChangePasswordPage.vue
+++ src/pages/ChangePasswordPage.vue
@@ -1,19 +1,14 @@
 <template>
-    <div>
-      <h1>Change Password</h1>
-      <form @submit.prevent="submitForm">
-        <div>
-          <label for="password">New Password:</label>
-          <input type="password" id="password" v-model="password">
-        </div>
-        <div>
-          <label for="confirmPassword">Confirm New Password:</label>
-          <input type="password" id="confirmPassword" v-model="confirmPassword">
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+   <div class="wrapper">
+        <h1>Change Password</h1>
+        <form @submit.prevent="submitForm">
+          <input type="password" id="password" v-model="password" placeholder="Password">
+          <input type="password" id="confirmPassword" v-model="confirmPassword" placeholder="Confirm Password"> 
+            <button type="submit">Submit</button>
+        </form><br>
     </div>
-  </template>
+</template>
+
   
   <script>
   import axios from 'axios';
@@ -53,3 +48,67 @@
   }
 }
 </script>
+
+<style scoped>
+.wrapper {
+    width: 330px;
+    padding: 2rem 1rem;
+    margin: 50px auto;
+    background-color: white;
+    border-radius: 10px;
+    text-align: center;
+    box-shadow: 0 20px 35px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    font-size: 2rem;
+    color: black;
+    margin-bottom: 1.2rem;
+}
+
+form input {
+    width: 70%;
+    outline: none;
+    padding: 1px solid white;
+    padding: 12px 20px;
+    margin-bottom: 10px;
+    border-radius: 20px;
+    background: #e4e4e4;
+}
+
+button {
+    font-size: 1rem;
+    margin-top: 1.8rem;
+    padding: 10px 0;
+    border-radius: 20px;
+    outline: none;
+    border: none;
+    width: 90%;
+    color: white;
+    background: crimson;
+    cursor: pointer;
+}
+
+button :hover {
+    background: black;
+}
+
+input:focus {
+    border: 1px solid rgb(192, 192, 192);
+}
+
+.alert {
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+}
+
+.alert-danger {
+    color: #a94442;
+    background-color: #f2dede;
+    border-color: #ebccd1;
+}
+
+
+</style>
--- src/pages/ForgotPasswordPage.vue
+++ src/pages/ForgotPasswordPage.vue
@@ -1,13 +1,10 @@
 <template>
-<div>
-      <h1>Change Password</h1>
-      <form @submit.prevent="submitForm">
-        <div>
-          <label for="email">Enter Email:</label>
-          <input type="email" id="email" v-model="email">
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+   <div class="wrapper">
+        <h1>Change Password</h1>
+        <form @submit.prevent="submitForm">
+            <input type="email" id="email" v-model="email" placeholder="Email">
+            <button type="submit">Submit</button>
+        </form><br>
     </div>
 </template>
 
@@ -34,3 +31,78 @@ export default {
 }
 }
 </script>
+
+<style scoped>
+.wrapper {
+    width: 330px;
+    padding: 2rem 1rem;
+    margin: 50px auto;
+    background-color: white;
+    border-radius: 10px;
+    text-align: center;
+    box-shadow: 0 20px 35px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    font-size: 2rem;
+    color: black;
+    margin-bottom: 1.2rem;
+}
+
+form input {
+    width: 70%;
+    outline: none;
+    padding: 1px solid white;
+    padding: 12px 20px;
+    margin-bottom: 10px;
+    border-radius: 20px;
+    background: #e4e4e4;
+}
+
+button {
+    font-size: 1rem;
+    margin-top: 1.8rem;
+    padding: 10px 0;
+    border-radius: 20px;
+    outline: none;
+    border: none;
+    width: 90%;
+    color: white;
+    background: crimson;
+    cursor: pointer;
+}
+
+button :hover {
+    background: black;
+}
+
+input:focus {
+    border: 1px solid rgb(192, 192, 192);
+}
+
+.account {
+    font-size: 0.8rem;
+    margin-top: 1.4rem;
+    color: #636363;
+}
+
+.account-link {
+    color: crimson;
+    text-decoration: none;
+}
+
+.alert {
+    padding: 10px;
+    margin-bottom: 20px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+}
+
+.alert-danger {
+    color: #a94442;
+    background-color: #f2dede;
+    border-color: #ebccd1;
+}
+
+
+</style>
