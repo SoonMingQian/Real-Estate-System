8fb9752b9eee398824565bf01cd6569435c53d5b
G00438454 <g00438454@atu.ie>
2024-10-26 15:19:06 +0100

    update apis

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/test/";
+const API_URL = "https://real-estate-system-q28g.onrender.com/api/test/";
 
 class UserService{
     getPublicContent(){
@@ -9,27 +9,27 @@ class UserService{
     }
 
     getSaleProperties(){
-        return axios.get('http://localhost:8080/api/properties/sale');
+        return axios.get('https://real-estate-system-q28g.onrender.com/api/properties/sale');
     }
 
     getRentProperties(){
-        return axios.get('http://localhost:8080/api/properties/rent');
+        return axios.get('https://real-estate-system-q28g.onrender.com/api/properties/rent');
     }
     
     getPropertyDetail(propertyId) {
-        return axios.get(`http://localhost:8080/api/property/${propertyId}`);
+        return axios.get(`https://real-estate-system-q28g.onrender.com/api/property/${propertyId}`);
     }
 
     getApartment(){
-        return axios.get('http://localhost:8080/api/apartment');
+        return axios.get('https://real-estate-system-q28g.onrender.com/api/apartment');
     }
 
     getCommercial(){
-        return axios.get('http://localhost:8080/api/office');
+        return axios.get('https://real-estate-system-q28g.onrender.com/api/office');
     }
 
     addProperty(userId, data){
-        return axios.post(`http://localhost:8080/api/add-property/${userId}`, data, { headers: authHeader() });
+        return axios.post(`https://real-estate-system-q28g.onrender.com/api/add-property/${userId}`, data, { headers: authHeader() });
     }
 
     
@@ -39,7 +39,7 @@ class UserService{
             formData.append('files', images[i].file);
         }
 
-        return axios.post(`http://localhost:8080/api/add-property/${propertyId}/upload`, formData, {
+        return axios.post(`https://real-estate-system-q28g.onrender.com/api/add-property/${propertyId}/upload`, formData, {
             headers: {
                 ...authHeader(),
                 'Content-Type': 'multipart/form-data'
@@ -48,11 +48,11 @@ class UserService{
     }
 
     getEditProperty(propertyId) {
-        return axios.get(`http://localhost:8080/api/property/${propertyId}`, { headers: authHeader() });
+        return axios.get(`https://real-estate-system-q28g.onrender.com/api/property/${propertyId}`, { headers: authHeader() });
     }
 
     editProperty(propertyId, data){
-        return axios.put(`http://localhost:8080/api/updateproperty/${propertyId}`, data, { headers: authHeader() });
+        return axios.put(`https://real-estate-system-q28g.onrender.com/api/updateproperty/${propertyId}`, data, { headers: authHeader() });
     }
 
     editPropertyImg(propertyId, images) {
@@ -61,7 +61,7 @@ class UserService{
             formData.append('files', images[i].file);
         }
     
-        return axios.post(`http://localhost:8080/api/add-property/${propertyId}/upload`, formData, {
+        return axios.post(`https://real-estate-system-q28g.onrender.com/api/add-property/${propertyId}/upload`, formData, {
             headers: {
                 ...authHeader(),
                 'Content-Type': 'multipart/form-data'
@@ -70,39 +70,39 @@ class UserService{
     }
 
     deleteFile(fileId){
-        return axios.delete(`http://localhost:8080/api/file/${fileId}`, { headers: authHeader() });
+        return axios.delete(`https://real-estate-system-q28g.onrender.com/api/file/${fileId}`, { headers: authHeader() });
     }
 
     deleteProperty(propertyId){
-        return axios.delete(`http://localhost:8080/api/deleteproperty/${propertyId}`, { headers: authHeader() });
+        return axios.delete(`https://real-estate-system-q28g.onrender.com/api/deleteproperty/${propertyId}`, { headers: authHeader() });
     }
 
     getMyProperties(userId){
-        return axios.get(`http://localhost:8080/api/user/${userId}`, { headers: authHeader() })
+        return axios.get(`https://real-estate-system-q28g.onrender.com/api/user/${userId}`, { headers: authHeader() })
     }
 
     getProfile(userId){
-        return axios.get(`http://localhost:8080/api/useraccount/${userId}`, { headers: authHeader() });
+        return axios.get(`https://real-estate-system-q28g.onrender.com/api/useraccount/${userId}`, { headers: authHeader() });
     }
 
     updateProfile(userId, userData){
-        return axios.put(`http://localhost:8080/api/updateuser/${userId}`, userData, { headers: authHeader() });
+        return axios.put(`https://real-estate-system-q28g.onrender.com/api/updateuser/${userId}`, userData, { headers: authHeader() });
     }
 
     addToShortlist(userId, propertyId){
-        return axios.post(`http://localhost:8080/api/users/shortlist/${userId}/${propertyId}`, { headers: authHeader() });
+        return axios.post(`https://real-estate-system-q28g.onrender.com/api/users/shortlist/${userId}/${propertyId}`, { headers: authHeader() });
     }
 
     getShortlistedProperties(userId){
-        return axios.get(`http://localhost:8080/api/shortlist/${userId}`, { headers: authHeader() });
+        return axios.get(`https://real-estate-system-q28g.onrender.com/api/shortlist/${userId}`, { headers: authHeader() });
     }
 
     removeFromShortlist(userId, propertyId){
-        return axios.delete(`http://localhost:8080/api/deleteshortlist/${userId}/${propertyId}`, { headers: authHeader() });
+        return axios.delete(`https://real-estate-system-q28g.onrender.com/api/deleteshortlist/${userId}/${propertyId}`, { headers: authHeader() });
     }
 
     getEmailByPropertyId(propertyId){
-        return axios.get(`http://localhost:8080/api/property/${propertyId}/userEmail`, { headers: authHeader() });
+        return axios.get(`https://real-estate-system-q28g.onrender.com/api/property/${propertyId}/userEmail`, { headers: authHeader() });
     }
 
 }
